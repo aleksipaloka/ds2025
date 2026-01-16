babae9e1bb5823ed82e3f377272b1a47330a87d0
aleksipaloka <it2022081@hua.gr>
2026-01-16 21:38:42 +0200

    Early Rest

--- ds2025/src/main/java/gr/hua/dit/ds2025/core/service/ReviewDataService.java
+++ ds2025/src/main/java/gr/hua/dit/ds2025/core/service/ReviewDataService.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface ReviewDataService {
 
     List<ReviewView> getAllReviews();
+
+    ReviewView getReviewById(long reviewId);
 }
--- ds2025/src/main/java/gr/hua/dit/ds2025/web/rest/ReviewResource.java
+++ ds2025/src/main/java/gr/hua/dit/ds2025/web/rest/ReviewResource.java
@@ -1,12 +1,15 @@
 package gr.hua.dit.ds2025.web.rest;
 
+import gr.hua.dit.ds2025.core.service.ReviewBusinessLogicService;
 import gr.hua.dit.ds2025.core.service.ReviewDataService;
+import gr.hua.dit.ds2025.core.service.model.CreateReviewRequest;
 import gr.hua.dit.ds2025.core.service.model.ReviewView;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,16 +19,22 @@ public class ReviewResource {
 
     private final ReviewDataService reviewDataService;
 
-    public ReviewResource(final ReviewDataService reviewDataService) {
-        if (reviewDataService == null) throw new NullPointerException();
+    public ReviewResource(ReviewDataService reviewDataService) {
         this.reviewDataService = reviewDataService;
     }
+
 
     @PreAuthorize("hasRole('INTEGRATION_READ')")
     @GetMapping("")
     public List<ReviewView> reviews() {
         final List<ReviewView> reviewViewList = this.reviewDataService.getAllReviews();
         return reviewViewList;
+    }
+
+    @PreAuthorize("hasRole('INTEGRATION_READ')")
+    @GetMapping("/{id}")
+    public ReviewView byId(@PathVariable("id") long id) {
+        return reviewDataService.getReviewById(id);
     }
 
 }
